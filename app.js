b9042b29e472fcc85c921a817eca418c3762d4da
DegelCR <franciscojavier.gonzalez5@gmail.com>
2024-01-20 20:35:48 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/*let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el n�mero');
+} else {
+if (numeroUsuario < numeroSecreto ){
+    alert(" el numero es mas grande");
+    numeroUsuario = prompt("Me indicas un n�mero por favor:");
+    } else {
+        alert(" el numero es mas pequeño");
+        numeroUsuario = prompt("Me indicas un n�mero por favor:");
+    }
+}*/
+
+
+
+//desafio 2
+
+let nombre = luna;
+let edad = 25;
+let numeroVentas = 50;
+let saldoDisponible = 1000;
+let mensajeError = "¡Error! Completa todos los campos!."
+
+nombre = prompt('Por favor digite su nombre'); 
+edad = prompt('Por favor digite su edad');
+
+alert("¡Bienvenida y bievenido a nuestro sitio web!.");
+
+if(edad >= 18){
+    alert('Ya puedes obtener tu licensia');
+} else {
+    alert('No estas listo para esta conversacion');
+}
+
+alert(mensajeError);
+
